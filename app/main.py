d74d775f6f935e5ef130e2892ebdd200f91b1482
Diamond <shahrzad.zolghadr@gmail.com>
2024-08-30 16:19:24 +0330

    the ride cansel prediction inference added to the endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import pickle
+from sklearn.ensemble import GradientBoostingClassifier
 
+
+class FeaturesIn(BaseModel):
+    C_Income: int
+    C_UID: int
+    Destination: int
+    C_Time:int
 
 app = FastAPI()
 
@@ -9,5 +17,10 @@ async def root():
     return {"message": "Welcome to the version 1.0 of Ride Canselation Prediction service!"}
 
 @app.post("/gradient_boosting_cls")
-async def gbc_predict(a, b, c, d):
-    return {"class": 0, "info":"Class 1 means the ride canselation is probable!"}
+async def gbc_predict(features: FeaturesIn):
+
+    with open("app/gbc.pkl", "rb") as f:
+        clf = pickle.load(f)
+
+    prediction = clf.predict([[features.C_UID, features.Destination, features.C_Time, features.C_Income]])[0]
+    return {"class": str(prediction), "info":"Class 1 means the ride canselation is probable!"}
